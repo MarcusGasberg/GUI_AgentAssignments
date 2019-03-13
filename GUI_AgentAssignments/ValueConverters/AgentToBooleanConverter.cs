a000e8b00413779ca7ebf26f030d06a2de5e9587
Marcus Gasberg <mrgasberg@hotmail.com>
2019-03-13 20:04:25 +0100

    Created more XML comments and fixed the Speciality list

--- GUI_AgentAssignments/ValueConverters/AgentToBooleanConverter.cs
+++ GUI_AgentAssignments/ValueConverters/AgentToBooleanConverter.cs
@@ -3,6 +3,9 @@ using System.Globalization;
 
 namespace GUI_AgentAssignments
 {
+    /// <summary>
+    /// Converts an Agent to a Boolean if that Agent is James Bond
+    /// </summary>
     public class AgentToBooleanConverter : BaseValueConverter<AgentToBooleanConverter>
     {
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
